2c190d6409c06f9acd00cfac137eb04149d0a3ef
Mickey Groff <mick.groff@gmail.com>
2019-12-02 12:11:38 -0700

    template-literals

--- js/index.js
+++ js/index.js
@@ -1,42 +1,122 @@
-const siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
-  },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
-  },
-  "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
-  },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
-};
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+  let nav = {
+    nav1: "Services",
+    nav2: "Product",
+    nav3: "Vision",
+    nav4: "Features",
+    nav5: "About",
+    nav6: "Contact",
+    imgsrc: "img/logo.png"
+  };
+  let cta = {
+    h1: "DOM </br>Is</br> Awesome",
+    button: "Get Started",
+    imgsrc: "img/header-img.png"
+  };
+  let maincontent = {
+    featuresh4:"Features",
+    featurescontent: "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
+    abouth4:"About",
+    aboutcontent: "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
+    middleimgsrc: "img/mid-page-accent.jpg",
+
+    servicesh4:"Services",
+    servicescontent: "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
+    producth4:"Product",
+    productcontent: "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
+    visionh4:"Vision",
+
+    visioncontent: "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  };
+
+  let contact = {
+    contacth4 : "Contact",
+    address : "123 Way 456 Street Somewhere, USA",
+    phone : "1 (888) 888-8888",
+    email : "sales@greatidea.io",
+  };
+  let footer = {
+    copyright : "Copyright Great Idea! 2018",
+  };
+function markup(){
+ return `<div class="container">
+
+        <header>
+            <nav>
+                <a href="#">${nav.nav1}</a>
+                <a href="#">${nav.nav2}</a>
+                <a href="#">${nav.nav3}</a>
+                <a href="#">${nav.nav4}</a>
+                <a href="#">${nav.nav5}</a>
+                <a href="#">${nav.nav6}</a>
+            </nav>
+
+            <img id="logo-img" class="logo" src=${nav.imgsrc} alt="Great Idea! Company logo.">
+        </header>
+
+        <section class="cta">
+            <div class="cta-text">
+                <h1>${cta.h1}</h1>
+                <button>${cta.button}</button>
+            </div>
+            <img id="cta-img" src=${cta.imgsrc} alt="Image of a code snippet.">
+        </section>
+
+        <section class="main-content">
+
+            <div class="top-content">
+                <div class="text-content">
+                    <h4>${maincontent.featuresh4}</h4>
+                    <p>${maincontent.featurescontent}</p>
+                </div>
+                <div class="text-content">
+                    <h4>${maincontent.abouth4}</h4>
+                    <p>${maincontent.aboutcontent}</p>
+                </div>
+            </div>
+
+            <img class="middle-img" id="middle-img" src=${maincontent.middleimgsrc} alt="Image of code snippets across the screen">
+
+            <div class="bottom-content">
+                <div class="text-content">
+                    <h4>${maincontent.servicesh4}</h4>
+                    <p>${maincontent.servicescontent}</p>
+                </div>
+                <div class="text-content">
+                    <h4>${maincontent.producth4}</h4>
+                    <p>${maincontent.productcontent}</p>
+                </div>
+                <div class="text-content">
+                    <h4>${maincontent.visionh4}</h4>
+                    <p>${maincontent.visioncontent}</p>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="contact">
+
+            <h4>${contact.contacth4}</h4>
+
+            <p>${contact.address}</p>
+
+            <p>${contact.phone}</p>
+
+            <p>${contact.email}</p>
+
+        </section>
+
+        <footer>
+            <p>${footer.copyright}</p>
+        </footer>
+
+    </div>`}
+
+
+console.log(footer.copyright)
+let go = markup();
+document.body.innerHTML = go;
