be625816b18179df9123d6edd30a8d11d657b483
Mickey Groff <mick.groff@gmail.com>
2019-12-02 16:01:53 -0700

    finished-timer-stretch-goals finished-dom-with-selectors

--- js/index.js
+++ js/index.js
@@ -1,122 +1,119 @@
-
-  let nav = {
-    nav1: "Services",
-    nav2: "Product",
-    nav3: "Vision",
-    nav4: "Features",
-    nav5: "About",
-    nav6: "Contact",
+let nav = {
+    navarr: ["Services",
+    "Product",
+    "Vision",
+    "Features",
+    "About",
+    "Contact"],
     imgsrc: "img/logo.png"
-  };
-  let cta = {
-    h1: "DOM </br>Is</br> Awesome",
+};
+let cta = {
+    h1: "DOM</br>Is</br>Awesome",
     button: "Get Started",
     imgsrc: "img/header-img.png"
-  };
-  let maincontent = {
-    featuresh4:"Features",
+};
+let maincontent = {
+    featuresh4: "Features",
     featurescontent: "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
 
-    abouth4:"About",
+    abouth4: "About",
     aboutcontent: "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
 
     middleimgsrc: "img/mid-page-accent.jpg",
 
-    servicesh4:"Services",
+    servicesh4: "Services",
     servicescontent: "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
 
-    producth4:"Product",
+    producth4: "Product",
     productcontent: "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
 
-    visionh4:"Vision",
+    visionh4: "Vision",
 
     visioncontent: "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  };
+};
 
-  let contact = {
-    contacth4 : "Contact",
-    address : "123 Way 456 Street Somewhere, USA",
-    phone : "1 (888) 888-8888",
-    email : "sales@greatidea.io",
-  };
-  let footer = {
-    copyright : "Copyright Great Idea! 2018",
-  };
-function markup(){
- return `<div class="container">
-
-        <header>
-            <nav>
-                <a href="#">${nav.nav1}</a>
-                <a href="#">${nav.nav2}</a>
-                <a href="#">${nav.nav3}</a>
-                <a href="#">${nav.nav4}</a>
-                <a href="#">${nav.nav5}</a>
-                <a href="#">${nav.nav6}</a>
-            </nav>
-
-            <img id="logo-img" class="logo" src=${nav.imgsrc} alt="Great Idea! Company logo.">
-        </header>
-
-        <section class="cta">
-            <div class="cta-text">
-                <h1>${cta.h1}</h1>
-                <button>${cta.button}</button>
-            </div>
-            <img id="cta-img" src=${cta.imgsrc} alt="Image of a code snippet.">
-        </section>
-
-        <section class="main-content">
-
-            <div class="top-content">
-                <div class="text-content">
-                    <h4>${maincontent.featuresh4}</h4>
-                    <p>${maincontent.featurescontent}</p>
-                </div>
-                <div class="text-content">
-                    <h4>${maincontent.abouth4}</h4>
-                    <p>${maincontent.aboutcontent}</p>
-                </div>
-            </div>
-
-            <img class="middle-img" id="middle-img" src=${maincontent.middleimgsrc} alt="Image of code snippets across the screen">
-
-            <div class="bottom-content">
-                <div class="text-content">
-                    <h4>${maincontent.servicesh4}</h4>
-                    <p>${maincontent.servicescontent}</p>
-                </div>
-                <div class="text-content">
-                    <h4>${maincontent.producth4}</h4>
-                    <p>${maincontent.productcontent}</p>
-                </div>
-                <div class="text-content">
-                    <h4>${maincontent.visionh4}</h4>
-                    <p>${maincontent.visioncontent}</p>
-                </div>
-            </div>
-
-        </section>
-
-        <section class="contact">
-
-            <h4>${contact.contacth4}</h4>
-
-            <p>${contact.address}</p>
-
-            <p>${contact.phone}</p>
-
-            <p>${contact.email}</p>
-
-        </section>
-
-        <footer>
-            <p>${footer.copyright}</p>
-        </footer>
-
-    </div>`}
+let contact = {
+    contacth4: "Contact",
+    one: "123 Way 456 Street Somewhere, USA",
+    two: "1 (888) 888-8888",
+    three: "sales@greatidea.io",
+};
+let footer = {
+    copyright: "Copyright Great Idea! 2018",
+};
 
 
-console.log(footer.copyright)
-let go = markup();
-document.body.innerHTML = go;
+window.onload = function(){
+
+  let img1 = document.getElementById('cta-img');
+  img1.src = cta.imgsrc;
+  let img2 = document.getElementById('logo-img');
+    img2.src =nav.imgsrc;
+  let img3 = document.getElementById('middle-img');
+  img3.src = maincontent.middleimgsrc;
+// first();
+second();
+}
+let newEle;
+function first(){
+newEle = document.createElement('div');
+  return document.body.appendChild('newEle');
+}
+function second(){
+let parr = [maincontent.featurescontent,
+maincontent.aboutcontent,
+maincontent.servicescontent,
+maincontent.productcontent,
+maincontent.visioncontent,contact.one,contact.two,contact.three,footer.copyright];
+
+let h4arr = [maincontent.featuresh4,maincontent.abouth4,maincontent.servicesh4,maincontent.producth4,maincontent.visionh4,contact.contacth4];
+let con = document.getElementsByClassName('container');
+let header = document.getElementsByTagName('header');
+let nava = document.getElementsByTagName('nav');
+let arr = document.querySelectorAll('a');
+let mainc = document.querySelector('.main-content');
+let mainp = document.querySelectorAll('p');
+  let count = 0;
+let h4 = document.querySelectorAll('h4');
+let cont = document.getElementsByClassName('contact');
+console.log(cont)
+let pz;
+
+
+arr.forEach(function (a) {
+    a.textContent = nav.navarr[count];
+    count++;
+    console.log(a.textContent)
+})
+
+count = 0;
+let h1 = document.querySelector('h1');
+let ctaText = document.querySelector('cta-text');
+h1.innerHTML = (cta.h1);
+let button = document.querySelector('button');
+
+button.textContent = cta.button;
+
+h4.forEach(function(h){
+    h.textContent = h4arr[count];
+       count++;
+    console.log(h.textContent)
+})
+
+count = 0;
+mainp.forEach(function(p){
+    p.textContent = parr[count];
+    count++;
+});
+
+}
+
+
+function styleIt(el){
+  mainc.appendChild(el)
+el.styles.width = '100px',
+  el.styles.height = '100px',
+  el.styles.background = 'red',
+  el.styles.position = 'absolute',
+  el.styles.top = '100px';
+}
